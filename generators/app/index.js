d82cd9d94ddccb5744ae90e25b1174296faf4ff8
Rodrigo Valerio <rsvalerio@gmail.com>
2016-08-31 17:13:14 +0100

    Prompts moved to another js file.

--- generators/app/index.js
+++ generators/app/index.js
@@ -2,20 +2,13 @@
 var yeoman = require('yeoman-generator');
 var chalk = require('chalk');
 var yosay = require('yosay');
+var prompts = require('./lib/prompts');
 
 module.exports = yeoman.Base.extend({
   prompting: function () {
-    // Have Yeoman greet the user.
     this.log(yosay(
-      'Welcome to the solid ' + chalk.red('generator-camel') + ' generator!'
+      chalk.red('Apache Camel') + ' generator!'
     ));
-
-    var prompts = [{
-      type: 'confirm',
-      name: 'someAnswer',
-      message: 'Would you like to enable this option?',
-      default: true
-    }];
 
     return this.prompt(prompts).then(function (props) {
       // To access props later use this.props.someAnswer;
@@ -31,6 +24,6 @@ module.exports = yeoman.Base.extend({
   },
 
   install: function () {
-    this.installDependencies();
+    // this.installDependencies();
   }
 });
---
+++ generators/app/lib/prompts.js
@@ -0,0 +1,33 @@
+/**
+ * Created by rsvalerio on 31/8/16.
+ */
+
+var path = require('path');
+var pkg = require(path.join(__dirname, '../', '../', '../', 'package.json'));
+var appName = pkg.name;
+
+module.exports = [
+  {
+    type: 'input',
+    name: 'appName',
+    message: 'App name: ',
+    default: appName
+  }, {
+    type: 'input',
+    name: 'package',
+    message: 'Package name: ',
+    default: 'com.' + appName
+  },
+  {
+    type: 'confirm',
+    name: 'docker',
+    message: 'Dockerize the app?',
+    default: true
+  },
+  {
+    type: 'confirm',
+    name: 'hal',
+    message: 'Install the Hal Browser?',
+    default: true
+  }
+];
